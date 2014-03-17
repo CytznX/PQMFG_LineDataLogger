f7c33d320e6a7f439b8df71bc86b4e3d604e600f
Max Seifert <maxs@pqmfg.com>
2014-03-17 18:02:26 -0400

    changed rpi from True to False so i can test on anny comp

--- StateMachine.py
+++ StateMachine.py
@@ -4,7 +4,7 @@ This is Version 2.0 of the PQMFG state machine feature improved as well as simpl
 Created By: Maxwell Seifert
 
 '''
-rpi = True 
+rpi = False 
 
 #Import the needed moduals
 import time, datetime, string, socket
