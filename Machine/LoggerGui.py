e760a153dbb416c79eae4dca5655450d890f2733
Maxwell <maxs@pqmfg.com>
2014-07-31 14:15:51 -0400

    added full screen toggle
    "cause im awesome"

--- Machine/LoggerGui.py
+++ Machine/LoggerGui.py
@@ -56,6 +56,30 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (200, 200, 200)
 
+_fullscreen = False
+
+#FullScreen
+def toggle_fullscreen():
+	screen = pygame.display.get_surface()
+	tmp = screen.convert()
+	caption = pygame.display.get_caption()
+	cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007
+
+	w,h = screen.get_width(),screen.get_height()
+	flags = screen.get_flags()
+	bits = screen.get_bitsize()
+
+	pygame.display.quit()
+	pygame.display.init()
+
+	screen = pygame.display.set_mode((w,h),flags^FULLSCREEN,bits)
+	screen.blit(tmp,(0,0))
+	pygame.display.set_caption(*caption)
+
+	pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
+
+	pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
+
 #How I shutdown the pi
 def shutdownPI():
 	command = "/usr/bin/sudo /sbin/shutdown -h now"
@@ -148,6 +172,9 @@ def main():
 	#Sets screen resolution and title
 	DISPLAYSURFACE = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 	pygame.display.set_caption('PQMFG ActivityLogger ')
+
+	if _fullscreen:
+		toggle_fullscreen()
 
 	#Static Message stings & Deault/header fonts
 	#---------------------------------------------------------------------------------------------------
